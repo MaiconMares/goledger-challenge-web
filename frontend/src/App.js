77eca92e3f7d21a06b197c0d1b7852e9d96100a2
Maicon Mares <maiconmaresunb@gmail.com>
2020-02-25 16:34:55 -0300

    Adding React Router to define components paths.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import Routes from './routes';
 
 function App() {
   return (
     <>
+      <Sidebar/>
+      <Routes/>
     </> 
   );
 }
---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Sidebar from './components/Sidebar/Sidebar';
+import Contacts from './pages/Contacts';
+import Profile from './pages/Profile';
+
+function Routes() {
+    return (
+        <>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Dashboard}/>
+                    <Route path="/contacts" component={Contacts}/>
+                    <Route path="/profile" component={Profile}/>
+                </Switch>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default Routes;
