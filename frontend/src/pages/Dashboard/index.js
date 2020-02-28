6db88a3ad911edd6dad478a30f950531a4f01a49
Maicon Mares <maiconmaresunb@gmail.com>
2020-02-28 09:46:49 -0300

    Adding realtime number of contacts stored to Dashboard component.

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,22 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import $ from 'jquery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
+import api from "../../services/api";
+import video from "./../../assets/videos/background.mp4";
 
 function Dashboard() {
+    const [contacts, setContacts] = useState([]);
+
     useEffect(() => {
-        $('.count').each(function () {
-            $(this).prop('Counter',0).animate({
-                Counter: $(this).text()
-            }, {
-                duration: 4000,
-                easing: 'swing',
-                step: function (now) {
-                    $(this).text(Math.ceil(now));
+        async function updateContactAnimation() {
+            let data = {
+                "selector": {
+                "@assetType": "contact"
                 }
+            }
+            const response = await api.post('/search', data);
+            setContacts(response.data.result);
+
+            $('.count').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
             });
-        });
+        }
+        updateContactAnimation();
     }, []);
     
     return (
@@ -26,7 +41,7 @@ function Dashboard() {
             </div>
             <div className="contacts-info bg-dark">
                 <div className="contacts-body">
-                    <h1 className="count">200</h1>
+                    <h1 className="count">{contacts.length}</h1>
                     <h5>Registered contacts</h5>
                 </div>
                 <div className="contacts-footer">
@@ -35,59 +50,9 @@ function Dashboard() {
                 </div>
             </div>
 
-            <div className="contacts-info bg-dark">
-                <div className="contacts-body">
-                    <h1 className="count">200</h1>
-                    <h5>Registered contacts</h5>
-                </div>
-                <div className="contacts-footer">
-                    <i><FontAwesomeIcon icon={faArrowUp}/><span>25%</span></i>
-                    <span>increase in stored contacts</span>
-                </div>
-            </div>
-
-            <div className="contacts-info bg-dark">
-                <div className="contacts-body">
-                    <h1 className="count">200</h1>
-                    <h5>Registered contacts</h5>
-                </div>
-                <div className="contacts-footer">
-                    <i><FontAwesomeIcon icon={faArrowUp}/><span>25%</span></i>
-                    <span>increase in stored contacts</span>
-                </div>
-            </div>
-
-            <div className="contacts-info bg-dark">
-                <div className="contacts-body">
-                    <h1 className="count">200</h1>
-                    <h5>Registered contacts</h5>
-                </div>
-                <div className="contacts-footer">
-                    <i><FontAwesomeIcon icon={faArrowUp}/><span>25%</span></i>
-                    <span>increase in stored contacts</span>
-                </div>
-            </div>
-
-            <div className="contacts-info bg-dark">
-                <div className="contacts-body">
-                    <h1 className="count">200</h1>
-                    <h5>Registered contacts</h5>
-                </div>
-                <div className="contacts-footer">
-                    <i><FontAwesomeIcon icon={faArrowUp}/><span>25%</span></i>
-                    <span>increase in stored contacts</span>
-                </div>
-            </div>
-
-            <div className="contacts-info bg-dark">
-                <div className="contacts-body">
-                    <h1 className="count">200</h1>
-                    <h5>Registered contacts</h5>
-                </div>
-                <div className="contacts-footer">
-                    <i><FontAwesomeIcon icon={faArrowUp}/><span>25%</span></i>
-                    <span>increase in stored contacts</span>
-                </div>
+            <div className="background-video">
+                <div className="opacity-background"></div>
+                <video src={video} autoPlay muted loop></video>
             </div>
         </div>
     );
