e45378a063402fa5671a0bd809b1d6ac3c38210c
Maicon Mares <maiconmaresunb@gmail.com>
2020-02-28 09:49:45 -0300

    Making last adjustments.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import Sidebar from './components/Sidebar/Sidebar';
 import Contacts from './pages/Contacts';
 import Profile from './pages/Profile';
 
